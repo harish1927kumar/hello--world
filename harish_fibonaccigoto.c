0fd4cf1ed7df998858607faa6883606565e3939b
harishkumarbhagat <43933369+harishkumarbhagat@users.noreply.github.com>
2018-10-07 23:34:54 +0530

    Add files via upload

---
+++ harish_fibonaccigoto.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(int argc, char const*argv[])
+{
+	int a=1;
+	int b=1;
+	int c=0;
+	int i=0;
+	label_1:
+	if(i<10
+	{
+		a=b;
+		b=c;
+		c=a+b;
+		i=i+1;
+	}
+		else
+		{
+			goto label_2;
+		}
+printf("%d\n,",c);
+goto label_2;
+label_2:
+return 0;
+}
